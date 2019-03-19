150b4c3c691a3ec27f24c6086d8f8ac054525a4c
chesnutcase <chester8991@gmail.com>
2019-03-19 19:22:11 +0800

    add debug information in bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,5 +5,8 @@ include 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::create(__DIR__);
 $dotenv->load();
 
-Detrack\ElasticRoute\Plan::$defaultApiKey = getenv("elasticroute_api_key");
+Detrack\ElasticRoute\Plan::$defaultApiKey = getenv('elasticroute_api_key');
 Detrack\ElasticRoute\Plan::$baseURL = getenv('elasticroute_path');
+
+echo 'Default Api Key registered as: '.Detrack\ElasticRoute\Plan::$defaultApiKey;
+echo 'Base URL registered as: '.Detrack\ElasticRoute\Plan::$baseURL;
