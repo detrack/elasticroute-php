dac67dfe09a1f31abc59f826a07aec6c65c0e486
chesnutcase <chester8991@gmail.com>
2019-07-15 14:09:19 +0800

    add uploadvehicles function

---
+++ src/DashboardClient.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Detrack\ElasticRoute;
+
+use DateTime;
+
+class DashboardClient
+{
+    public static $baseUrl = 'https://app.elasticroute.com/api/v1/account';
+    public static $defaultApiKey = '';
+    public $apiKey = '';
+
+    /**
+     * Uploads the given stops to a certain date on the dashboard.
+     *
+     * @param Stop[] $stops       an array of stops to upload
+     * @param string $date        the date of which to upload the stops to
+     * @param mixed  $curlOptions
+     */
+    public function uploadStopsOnDate($stops, $date, $curlOptions = [])
+    {
+        $apiKey = $this->apiKey ?? static::$defaultApiKey;
+        $apiKey = trim($apiKey);
+        if ($apiKey == '') {
+            throw new BadFieldException('API Key is missing!');
+        }
+        if (DateTime::createFromFormat('Y-m-d', $date)->format('Y-m-d') !== $date) {
+            throw new BadFieldException('Please use YYYY-MM-DD as the format of the date!');
+        }
+        $endpoint = static::$baseUrl.'/'.'stops/'.$date.'/bulk';
+        $ch = curl_init($endpoint);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Authorization: Bearer '.$apiKey,
+        ]);
+        curl_setopt_array($ch, $curlOptions);
+
+        Stop::validateStops($stops);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(['data' => $stops]));
+        curl_setopt($ch, CURLOPT_POST, true);
+        $responseString = curl_exec($ch);
+        $error = curl_error($ch);
+        // @codeCoverageIgnoreStart
+        if ($error != '') {
+            throw new \RuntimeException('Curl Error occurred: '.$error);
+        }
+        // @codeCoverageIgnoreEnd
+        $httpCode = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+        if ($httpCode != '200') {
+            throw new \RuntimeException('API Return HTTP Code '.$httpCode.' : '.$responseString);
+        }
+        curl_close($ch);
+    }
+
+    /**
+     * Uploads the given vehicles onto the Dashboard.
+     *
+     * @param Vehicle[] $vehicles    an array of vehicles to upload
+     * @param mixed     $curlOptions
+     */
+    public function uploadVehicles($vehicles, $curlOptions = [])
+    {
+        $apiKey = $this->apiKey ?? static::$defaultApiKey;
+        $apiKey = trim($apiKey);
+        if ($apiKey == '') {
+            throw new BadFieldException('API Key is missing!');
+        }
+        $endpoint = static::$baseUrl.'/vehicles/bulk';
+        $ch = curl_init($endpoint);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Authorization: Bearer '.$apiKey,
+        ]);
+        curl_setopt_array($ch, $curlOptions);
+
+        Vehicle::validateVehicles($vehicles);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(['data' => $vehicles]));
+        curl_setopt($ch, CURLOPT_POST, true);
+        $responseString = curl_exec($ch);
+        $error = curl_error($ch);
+        // @codeCoverageIgnoreStart
+        if ($error != '') {
+            throw new \RuntimeException('Curl Error occurred: '.$error);
+        }
+        // @codeCoverageIgnoreEnd
+        $httpCode = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+        if ($httpCode != '200') {
+            throw new \RuntimeException('API Return HTTP Code '.$httpCode.' : '.$responseString);
+        }
+        curl_close($ch);
+    }
+}
---
+++ tests/integration/TestDashboardClient.php
@@ -0,0 +1,64 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Detrack\ElasticRoute\DashboardClient;
+
+final class TestDashboardClient extends TestCase
+{
+    protected $proxy = [];
+
+    public function setUp(): void
+    {
+        if (getenv('elasticroute_proxy_enabled') == 'true') {
+            $this->proxy = [
+                CURLOPT_PROXY => getenv('elasticroute_proxy_url'),
+                CURLOPT_HEADEROPT => CURLHEADER_UNIFIED,
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_SSL_VERIFYHOST => false,
+                CURLOPT_SSL_VERIFYSTATUS => false,
+            ];
+        }
+    }
+
+    public function testUploadStops()
+    {
+        $client = new DashboardClient();
+        $stops = [
+            [
+                'name' => 'SUTD',
+                'address' => '8 Somapah Road Singapore 487372',
+            ],
+            [
+                'name' => 'Changi Airport',
+                'address' => '80 Airport Boulevard (S)819642',
+            ],
+            [
+                'name' => 'Gardens By the Bay',
+                'address' => '18 Marina Gardens Drive Singapore 018953',
+            ],
+            [
+                'name' => 'Singapore Zoo',
+                'address' => '80 Mandai Lake Road Singapore 729826',
+            ],
+        ];
+        $client->apiKey = getenv('elasticroute_api_key');
+        DashboardClient::$baseUrl = 'https://staging.elasticroute.com/api/v1'.'/account';
+        $client->uploadStopsOnDate($stops, date('Y-m-d'), $this->proxy);
+    }
+
+    public function testUploadVehicles()
+    {
+        $client = new DashboardClient();
+        $client->apiKey = getenv('elasticroute_api_key');
+        DashboardClient::$baseUrl = 'https://staging.elasticroute.com/api/v1'.'/account';
+        $vehicles = [
+            [
+                'name' => 'Van 1',
+            ],
+            [
+                'name' => 'Van 2',
+            ],
+        ];
+        $client->uploadVehicles($vehicles, $this->proxy);
+    }
+}
