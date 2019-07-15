1cccc940885565c8c6b36ee0b58f619733b8fc08
chesnutcase <chester8991@gmail.com>
2019-07-15 14:04:26 +0800

    refactor class to use javabeans paradigm

--- src/Vehicle.php
+++ src/Vehicle.php
@@ -4,17 +4,34 @@ namespace Detrack\ElasticRoute;
 
 class Vehicle implements \JsonSerializable
 {
-    public $depot;
-    public $name;
-    public $priority = 1;
-    public $weight_capacity;
-    public $volume_capacity;
-    public $seating_capacity;
-    public $buffer;
-    public $avail_from;
-    public $avail_till;
-    public $return_to_depot = false;
-    public $vehicle_types;
+    /** @var array encapsulates the data of the model */
+    protected $data = [
+        'depot' => null,
+        'name' => null,
+        'priority' => 1,
+        'weight_capacity' => null,
+        'volume_capacity' => null,
+        'seating_capacity' => null,
+        'buffer' => null,
+        'avail_from' => null,
+        'avail_till' => null,
+        'return_to_depot' => false,
+        'vehicle_types' => null,
+        // dashboard only
+        'avail' => null,
+        'avail_mon' => null,
+        'avail_tue' => null,
+        'avail_wed' => null,
+        'avail_thu' => null,
+        'avail_fri' => null,
+        'avail_sat' => null,
+        'avail_sun' => null,
+        'zones' => null,
+        'groups' => null,
+    ];
+
+    /** @var mixed[] keeps track of the immediate previous value of the attribute */
+    protected $previousAttributeValues = [];
 
     public function __construct($data = [])
     {
@@ -23,21 +40,31 @@ class Vehicle implements \JsonSerializable
         }
     }
 
+    public function __set($key, $value)
+    {
+        // if this key exists in the data array
+        if (array_key_exists($key, $this->data)) {
+            // if there was a change in data values
+            if (is_null($this->data[$key]) || $this->data[$key] !== $value) {
+                // save it in previous attributes, just in case we need it later
+                $this->previousAttributeValues[$key] = $this->data[$key];
+                $this->data[$key] = $value;
+            }
+        }
+    }
+
+    public function __get($key)
+    {
+        return $this->data[$key];
+    }
+
     public function jsonSerialize()
     {
-        return [
-            'depot' => $this->depot,
-            'name' => $this->name,
-            'priority' => $this->priority,
-            'weight_capacity' => $this->weight_capacity,
-            'volume_capacity' => $this->volume_capacity,
-            'seating_capacity' => $this->seating_capacity,
-            'buffer' => $this->buffer,
-            'avail_from' => $this->avail_from,
-            'avail_till' => $this->avail_till,
-            'return_to_depot' => $this->return_to_depot,
-            'vehicle_types' => $this->vehicle_types,
-        ];
+        $callback = function ($k, $v) {
+            return !(is_null($v) && !array_key_exists($k, $this->previousAttributeValues));
+        };
+
+        return array_filter($this->data, $callback, ARRAY_FILTER_USE_BOTH);
     }
 
     public static function validateVehicles($vehicles)
@@ -63,7 +90,9 @@ class Vehicle implements \JsonSerializable
                 throw new BadFieldException('Vehicle name cannot be more than 255 chars', $vehicle);
             }
             //distinct
-            if (count(array_filter(array_column($vehicles, 'name'), function ($v) use ($vehicle) {
+            if (count(array_filter(array_map(function ($v) {
+                return ($v instanceof self | $v instanceof \stdClass) ? $v->name : $v["name"];
+            }, $vehicles), function ($v) use ($vehicle) {
                 return $v == $vehicle['name'];
             })) > 1) {
                 throw new BadFieldException('Vehicle name must be distinct', $vehicle);
