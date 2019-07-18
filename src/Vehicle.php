11688043ddc46c3bd673e903cb50ac1a521891a4
chesnutcase <chester8991@gmail.com>
2019-07-18 16:57:06 +0800

    add property docstrings to vehicle

--- src/Vehicle.php
+++ src/Vehicle.php
@@ -2,6 +2,32 @@
 
 namespace Detrack\ElasticRoute;
 
+/**
+ * @property string   $name            Name of the vehicle. Must be unique.
+ * @property string   $depot           Name of the depot that the driver collects its goods from. Must correspond to a depot with the same name.
+ * @property int      $priority        The priority of the vehicle. Higher priority ones will be dispatched first.
+ * @property float    $weight_capacity Allows you to specify the maximum weight of goods this vehicle can carry
+ * @property float    $volume_capacity Allows you to specify the maximum volume of goods this vehicle can carry
+ * @property float    $seating_capcity Allows you to specify the number of human staff this vehicle can seat
+ * @property int      $buffer          Allows you to specify the buffer time, the amount of travelling time in minutes to factor in for each additional stop.
+ * @property int      $avail_from      The starting time where the vehicle is available, represented as 24hr time.
+ * @property int      $avail_to        The ending time where the vehicle is available, represented as 24hr time.
+ * @property string[] $vehicle_types   An array of strings representing what vehicle types (classes) this vehicle is classified under
+ * @property bool     $return_to_depot Specify whether the vehicle requires to return to the depot. The time taken to return to depot will be considered if this field is set to true.
+ * @property bool     $avail           DASHBOARD ONLY: Availability of the vehicle to be included for planning
+ * @property bool     $avail_mon       DASHBOARD ONLY: Availability of the vehicle to be included for planning on Mondays
+ * @property bool     $avail_tue       DASHBOARD ONLY: Availability of the vehicle to be included for planning on Tuesdays
+ * @property bool     $avail_wed       DASHBOARD ONLY: Availability of the vehicle to be included for planning on Wednesdays
+ * @property bool     $avail_thu       DASHBOARD ONLY: Availability of the vehicle to be included for planning on Thursdays
+ * @property bool     $avail_fri       DASHBOARD ONLY: Availability of the vehicle to be included for planning on Fridays
+ * @property bool     $avail_sat       DASHBOARD ONLY: Availability of the vehicle to be included for planning on Saturdays
+ * @property bool     $avail_sun       DASHBOARD ONLY: Availability of the vehicle to be included for planning on Sundays
+ * @property string[] $groups          DASHBOARD ONLY: Specifies an array of groups this vehicle belongs to; Only stops in the same group as the vehicle can be assigned to the vehicle. Used to separate your runs.
+ * @property bool     $all_groups      DASHBOARD ONLY: Specifies whether this Vehicle is omnipotent and can serve all stops assigned to any group. Overrides $groups.
+ * @property string[] $zones           DASHBOARD ONLY: Specifies an array of geofenced zones where this Vehicle can only serve stops within this geographical zones. Used for geofencing.
+ * @property-read string   $created_at      DASHBOARD ONLY: UTC time when the stop was created, as seen in the dashboard
+ * @property-read string   $updated_at      DASHBOARD ONLY: UTC time when the stop was updated, as seen in the dashboard
+ */
 class Vehicle implements \JsonSerializable
 {
     /** @var array encapsulates the data of the model */
@@ -91,7 +117,7 @@ class Vehicle implements \JsonSerializable
             }
             //distinct
             if (count(array_filter(array_map(function ($v) {
-                return ($v instanceof self | $v instanceof \stdClass) ? $v->name : $v["name"];
+                return ($v instanceof self | $v instanceof \stdClass) ? $v->name : $v['name'];
             }, $vehicles), function ($v) use ($vehicle) {
                 return $v == $vehicle['name'];
             })) > 1) {
